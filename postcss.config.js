f3ce35b5ccf2b31591517c39085d3778972cc666
Urs Grupp <urs@21sieben.de>
2022-01-06 01:48:53 +0100

    fix: fix postcss nesting with tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   plugins: [
     "postcss-import",
+    "tailwindcss/nesting",
     "tailwindcss",
     [
       "postcss-preset-env",
       {
         stage: 1,
+        features: { "nesting-rules": false },
       },
     ],
   ],
