4ed9d01cfbafd887bc90f6b547938458f2a150a2
Urs Grupp <urs@21sieben.de>
2022-01-19 16:28:47 +0100

    feat: implement sticky topbar

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,12 +1,19 @@
+import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import Headroom from "react-headroom";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import ArrowsRightIcon from "../assets/icons/arrows-right.svg";
 import MenuIcon from "../assets/icons/menu.svg";
 import staticData from "../data/static.json";
 import { NEXT_IMAGE_DEFAULT_QUALITY } from "../lib/constants";
-import { menuOpenState, topbarHeightState } from "../lib/state";
+import {
+  menuOpenState,
+  scrollLockState,
+  topbarHeightState,
+} from "../lib/state";
+import styles from "../styles/topbar.module.css";
 import Contact from "./contact";
 import Container from "./container";
 
@@ -40,60 +47,74 @@ const Topbar: React.FC<TopbarProps> = ({ data }) => {
     setMenuOpen(!menuOpen);
   };
 
+  const scrollLock = useRecoilValue(scrollLockState);
+  const scrollLockStyle = {
+    "--scroll-lock-width": scrollLock,
+  } as React.CSSProperties;
+
   return (
-    <header className="bg-white pt-20 pb-30 md:py-30" ref={topbarRef}>
-      <Container>
-        <div className="grid grid-cols-[1fr,1fr,1fr] gap-x-20">
-          <div>
-            {/* Menu toggler */}
-            <button
-              className="md:hidden flex flex-col items-center gap-15 text-19 font-fracture font-normal text-red-ci"
-              onClick={menuTogglerButtonHandler}
-            >
-              {/* Icon */}
-              <MenuIcon className="h-40 w-40" />
+    <Headroom className="relative z-40">
+      <header
+        className={classNames("bg-white pt-20 pb-30 md:py-30", styles.inner)}
+        ref={topbarRef}
+        style={scrollLockStyle}
+      >
+        <Container>
+          <div className="grid grid-cols-[1fr,1fr,1fr] gap-x-20">
+            <div>
+              {/* Menu toggler */}
+              <button
+                className="md:hidden flex flex-col items-center gap-15 text-19 font-fracture font-normal text-red-ci"
+                onClick={menuTogglerButtonHandler}
+              >
+                {/* Icon */}
+                <MenuIcon className="h-40 w-40" />
 
-              {/* Label */}
-              <span className="[writing-mode:vertical-rl]">
-                <span className="block rotate-180">Menü</span>
-              </span>
-            </button>
+                {/* Label */}
+                <span className="[writing-mode:vertical-rl]">
+                  <span className="block rotate-180">Menü</span>
+                </span>
+              </button>
 
-            {/* Menu */}
-            <nav className="hidden md:flex flex-col gap-y-6 w-fit">
-              {menu.map(({ href, label }) => (
-                <Link href={href} key={href}>
-                  <a className="flex items-center justify-between gap-20 md:text-28 lg:text-30 leading-none font-fracture font-normal transition-colors hover:text-red-ci group">
-                    <span>{label}</span>
-                    <ArrowsRightIcon className="opacity-0 group-hover:opacity-100 transition-opacity block h-[33px] w-[33px]" />
-                  </a>
-                </Link>
-              ))}
-            </nav>
-          </div>
+              {/* Menu */}
+              <nav className="hidden md:flex flex-col gap-y-6 w-fit">
+                {menu.map(({ href, label }) => (
+                  <Link href={href} key={href}>
+                    <a className="flex items-center justify-between gap-20 md:text-28 lg:text-30 leading-none font-fracture font-normal transition-colors hover:text-red-ci group">
+                      <span>{label}</span>
+                      <ArrowsRightIcon className="opacity-0 group-hover:opacity-100 transition-opacity block h-[33px] w-[33px]" />
+                    </a>
+                  </Link>
+                ))}
+              </nav>
+            </div>
 
-          {/* Logo */}
-          <Link href="/">
-            <a className="w-[200px] md:w-[305px] block">
-              <Image
-                quality={NEXT_IMAGE_DEFAULT_QUALITY}
-                src="/images/logo.svg"
-                alt="Zur Mühle"
-                width={305.232}
-                height={170.052}
-                layout="responsive"
-                priority={true}
+            {/* Logo */}
+            <Link href="/">
+              <a className="w-[200px] md:w-[305px] block">
+                <Image
+                  quality={NEXT_IMAGE_DEFAULT_QUALITY}
+                  src="/images/logo.svg"
+                  alt="Zur Mühle"
+                  width={305.232}
+                  height={170.052}
+                  layout="responsive"
+                  priority={true}
+                />
+              </a>
+            </Link>
+
+            {/* Contact */}
+            <div className="hidden md:block justify-self-end">
+              <Contact
+                opentimes__html={opentimes__html}
+                phoneLink={phoneLink}
               />
-            </a>
-          </Link>
-
-          {/* Contact */}
-          <div className="hidden md:block justify-self-end">
-            <Contact opentimes__html={opentimes__html} phoneLink={phoneLink} />
+            </div>
           </div>
-        </div>
-      </Container>
-    </header>
+        </Container>
+      </header>
+    </Headroom>
   );
 };
 
